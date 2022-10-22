80448b3d0a30d7257d6b7617985a998f2fd2f3fc
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-22 11:17:09 +0500

    08

--- List08.py
+++ List08.py
@@ -1,3 +1,6 @@
+from re import A
+
+
 def main(fruits):
     """
     A list called "fruits" is given and is five in length and contains at least one "apple". Remove the apples from the list and return the list.
@@ -6,4 +9,14 @@ def main(fruits):
     Returns:
         list: return answer
     """
-    return
+    ##a=fruits.count("apple")
+   # b=5-a
+    
+    i=0
+    ans=[]
+    while i<len(fruits):
+        if fruits[i]!="apple":
+            ans.append(fruits[i])
+        i+=1
+    return ans
+print(main(["apple", "banana", "apple",  "apple"]))    
